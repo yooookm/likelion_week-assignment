1d7ff127389fa57eff91e227cbe116ace4bcd539
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-05-01 22:53:04 +0900

    init: project

---
+++ src/main/java/study/likelionbeweekly/LikelionBeWeeklyApplication.java
@@ -0,0 +1,13 @@
+package study.likelionbeweekly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionBeWeeklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionBeWeeklyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/likelionbeweekly/LikelionBeWeeklyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.likelionbeweekly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionBeWeeklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
