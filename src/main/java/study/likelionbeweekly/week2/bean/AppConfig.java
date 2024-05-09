6547844c9a182345b462e0b2b9bbf1472b901218
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-05-09 17:29:16 +0900

    feat:빈 생성

---
+++ src/main/java/study/likelionbeweekly/week2/bean/AppConfig.java
@@ -0,0 +1,18 @@
+package study.likelionbeweekly.week2.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public MyBean myBean() {
+        return new MyBean();
+    }
+}
+
+class MyBean {
+    public void getBeanInfo() {
+        System.out.println("I am bean");
+    }
+}
