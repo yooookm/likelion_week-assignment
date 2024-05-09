c25e375215854f12eea986601ebccf361ad6c505
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-05-09 17:31:11 +0900

    feat:컴포넌트 빈 생성

---
+++ src/main/java/study/likelionbeweekly/week2/bean/ComponentBean.java
@@ -0,0 +1,10 @@
+package study.likelionbeweekly.week2.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentBean {
+    public void action() {
+        System.out.println("i am component bean");
+    }
+}
