70eb7fb7db4ab795aab864b3ee77c98670e0581f
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-05-19 01:48:19 +0900

    feale: InMemoryUserRepository 주석 추가

--- src/main/java/study/likelionbeweekly/week3/repository/InMemoryUserRepository.java
+++ src/main/java/study/likelionbeweekly/week3/repository/InMemoryUserRepository.java
@@ -8,11 +8,14 @@ import study.likelionbeweekly.week3.domain.User;
 @Repository
 public class InMemoryUserRepository implements UserRepository {
 
+    // DB 생성
     private static final Map<String, User> users = new HashMap<>();
 
+    /*  UserRepository 인터페이스로 함수 구현 강제  */
     @Override
     public void save(User user) {
         final String id = user.getId();
+        // DB 추가
         users.put(id, user);
     }
 
