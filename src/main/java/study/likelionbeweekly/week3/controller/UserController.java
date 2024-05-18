ab5016c81435a7f70a568e5664aa581422f19bc3
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-05-19 01:32:30 +0900

    feat: UserController url 매핑

--- src/main/java/study/likelionbeweekly/week3/controller/UserController.java
+++ src/main/java/study/likelionbeweekly/week3/controller/UserController.java
@@ -1,17 +1,23 @@
 package study.likelionbeweekly.week3.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import study.likelionbeweekly.week3.service.UserService;
 
-// 이곳에 컨트롤러 빈을 등록해보세요.
+@Controller
 public class UserController {
 
-	// 이곳에 UserService 를 주입 받아 보세요.
+	@Autowired
+	private UserService userService;
 
 	/**
 	 * 이곳에 GET 요청을 받아오는 애노테이션을 작성해 보세요.
 	 * 요청 주소는 "/" 입니다.
 	 */
+	@GetMapping("/")
 	public String loginPage() {
 		return "signIn";
 	}
@@ -20,6 +26,7 @@ public class UserController {
 	 * 이곳에 GET 요청을 받아오는 애노테이션을 작성해 보세요.
 	 * 요청 주소는 "/joinPage" 입니다.
 	 */
+	@GetMapping("/joinPage")
 	public String joinPage() {
 		return "signUp";
 	}
@@ -28,6 +35,7 @@ public class UserController {
 	 * 이곳에 POST 요청을 받아오는 애노테이션을 작성해 보세요.
 	 * 요청 주소는 "/join" 입니다.
 	 */
+	@GetMapping("/join")
 	public String join(@RequestParam("id") String id,
 					   @RequestParam("password") String password) {
 
@@ -39,6 +47,7 @@ public class UserController {
 	 * 이곳에 POST 요청을 받아오는 애노테이션을 작성해 보세요.
 	 * 요청 주소는 "/login" 입니다.
 	 */
+	@GetMapping("/login")
 	public String login(@RequestParam("id") String id,
 						@RequestParam("password") String password, Model model) {
 
