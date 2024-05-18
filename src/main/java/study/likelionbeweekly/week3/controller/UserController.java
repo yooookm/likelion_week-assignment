7ef8e6579d8ab0349ea9d734759503a0b3c37efd
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-05-19 02:02:52 +0900

    feat:Userexception handler 추가

--- src/main/java/study/likelionbeweekly/week3/controller/UserController.java
+++ src/main/java/study/likelionbeweekly/week3/controller/UserController.java
@@ -3,6 +3,7 @@ package study.likelionbeweekly.week3.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,5 +41,12 @@ public class UserController {
                         @RequestParam("password") String password, Model model) {
         userService.login(id, password, model);
         return "info";
+    }
+
+
+    @ExceptionHandler(IllegalStateException.class)
+    public String handleException(IllegalStateException e, Model model) {
+        model.addAttribute("error", e.getMessage());
+        return "error";
     }
 }
